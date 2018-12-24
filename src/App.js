66551455f4d189c374038e586f1927e089dba4f6
Victor Del Valle <vdelvalle11@gmail.com>
2018-12-24 18:38:06 +0000

    added event listener to increment and decrement

--- src/App.js
+++ src/App.js
@@ -39,19 +39,37 @@ const Player = (props) => {
         {props.name}
         </span>
 
-        <Counter score = {props.score}/>
+        <Counter />
       </div>
     )
 }
 
-const Counter = (props) => {
+class Counter extends Component {
+  state ={
+    score: 0
+  }
+
+  incrementScore = () => {
+    this.setState({
+      score: this.state.score + 1
+    });
+  }
+
+  decrementScore = () => {
+    this.setState({
+      score: this.state.score - 1
+    });
+  }
+
+  render(){
     return (
         <div className = "counter">
-          <button className="counter-action decrement">-</button>
-          <span className="counter-score">{props.score}</span>
-          <button className="counter-action increment">+</button>
+          <button className="counter-action decrement" onClick={this.decrementScore}>-</button>
+          <span className="counter-score">{this.state.score}</span>
+          <button className="counter-action increment" onClick={this.incrementScore}>+</button>
         </div>
     )
+  }
 }
 
 const AppContent = (props) => {
@@ -62,7 +80,6 @@ const AppContent = (props) => {
       {props.initialplayers.map( player =>
         <Player
           name={player.name}
-          score={player.score}
           key={player.id.toString()}
         />
       )}
